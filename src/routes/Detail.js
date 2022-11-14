178e53126434763be406a01caa951e380d2aa0c4
iamilly23 <iamilly@naver.com>
2022-11-15 01:48:09 +0900

    code

---
+++ src/routes/Detail.js
@@ -0,0 +1,30 @@
+import React from "react";
+import './Detail.css';
+
+class Detail extends React.Component {
+    componentDidMount() {
+        const { location, history } = this.props;
+        if (location.state === undefined) {
+            history.push('/');
+        }
+    }
+
+
+    render() {
+        const { location } = this.props;
+        if (location.state) {
+            return (
+                <div className="detail">
+                    <div className="detail__data">
+                        <h3 className="detail__title">{location.state.title}</h3>
+                        <h5 className="detail__year">{location.state.year}</h5>
+                        <p className="detail__summary">{location.state.summary}</p>
+                    </div>
+                </div>
+            );
+        } else {
+            return null;
+        }
+    }
+}
+export default Detail;
